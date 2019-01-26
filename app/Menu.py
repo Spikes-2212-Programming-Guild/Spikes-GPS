652c6bb89f55ad9713b0baa006afa853a88f2a16
uriahrokach <uriahrokach@gmail.com>
2019-01-26 15:46:12 +0200

    added Menu.py file

---
+++ app/Menu.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+def draw_menu():
+    pygame.init()
+    screen = pygame.display.set_mode((400, 300))
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+    pygame.display.flip()
+
+
