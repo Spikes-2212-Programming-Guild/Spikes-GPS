16737a44d201eda1beed57182692a4237331c569
uriahrokach <uriahrokach@gmail.com>
2019-01-26 21:08:12 +0200

    added basic form

--- app/Menu.py
+++ app/Menu.py
@@ -1,16 +1,26 @@
-import pygame
+import tkinter as tk
+
+
+class Window(tk.Frame):
+
+    def __init__(self, root):
+        tk.Frame.__init__(self, master=root)
+        self.master = root
+
+        self.master.title = "Spikes GPU"
+        self.pack(fill=tk.BOTH, expand=1)
 
 
 def draw_menu():
-    pygame.init()
-    screen = pygame.display.set_mode((400, 300))
-    done = False
+    root = tk.Tk()
+    root.geometry("400x300")
 
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
+    choise = tk.StringVar()
+    red_rbutton = tk.Radiobutton(root, text="Red", value="red", variable=choise).pack()
+    blue_rbutton = tk.Radiobutton(root, text="Blue", value="blue", variable=choise).pack()
 
-    pygame.display.flip()
+    confirm_button = tk.Button(root, text="confirm", command=lambda: root.destroy()).pack()
+    win = Window(root=root)
 
-
+    root.mainloop()
+    return choise.get()
