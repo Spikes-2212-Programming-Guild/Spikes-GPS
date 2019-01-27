ffd82a3a4a6325289d231c511c43d427ebd8b100
uriahrokach <uriahrokach@gmail.com>
2019-01-27 16:22:28 +0200

    added spikes logo to window

--- app/Menu.py
+++ app/Menu.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 
 
 class Window(tk.Frame):
@@ -14,8 +15,16 @@ def draw_menu():
     root.geometry("400x300")
     root.title("Spikes GPU")
     root.iconbitmap("sprites/Logo.ico")
+
+    img = ImageTk.PhotoImage(Image.open("sprites/spikes.png"))
+    panel = tk.Label(root, image=img)
+    panel.pack()
+
     choise = tk.StringVar()
-    red_rbutton = tk.Radiobutton(root, text="Red", value="red", variable=choise).pack()
+
+    red_rbutton = tk.Radiobutton(root, text="Red", value="red", variable=choise)
+    red_rbutton.pack()
+
     blue_rbutton = tk.Radiobutton(root, text="Blue", value="blue", variable=choise).pack()
 
     confirm_button = tk.Button(root, text="confirm", command=lambda: root.destroy()).pack()
