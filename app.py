b409f57597fce5f4ebd567aa74f2da60f4dec463
uriahrokach <uriahrokach@gmail.com>
2019-01-30 18:35:54 +0200

    added app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from src import map, data as nt, manu
+
+
+def main():
+    robot_width = 0.96
+    robot_length = 0.86
+
+    color = manu.draw_menu()
+    map.draw_field(robot_length=robot_length, robot_width=robot_width, color=color, data_function=nt.get_data)
+
+
+if __name__ == '__main__':
+    main()
