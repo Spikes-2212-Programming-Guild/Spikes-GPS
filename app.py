c10634b724149b37f1ae095e42a011831d584df1
T <tuval841@gmail.com>
2019-03-08 15:42:07 +0200

    merge sps

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from src import map, data as nt, menu
+from src import menu, map
+import odometry
+import time
+import pygame
 
 
 def main():
-    robot_width = 0.7
-    robot_length = 0.8
-
     color = menu.draw_menu()
-    map.draw_field(robot_length=robot_length, robot_width=robot_width, color=color, data_function=nt.get_data)
+    map.init_field(color)
+
+    handler = odometry.OdometryHandler(0, 0, 0, "width", "length", "Robot x", "Robot y", "Robot angle")
+    done = False
+    while not done:
+        handler.calculate()
+        time.sleep(0.04)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
 
 
 if __name__ == '__main__':
